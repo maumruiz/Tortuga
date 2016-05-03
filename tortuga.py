bdea525ce136c07e553ebcd422e695e576660061
maumruiz <mau.mruiz@hotmail.com>
2016-05-03 11:41:47 -0500

    Comentarios en tortuga.py

--- tortuga.py
+++ tortuga.py
@@ -1,6 +1,10 @@
 import sys
 
 #Tortuga scanner y parser
+# Archivo principal que lee un archivo para hacer su
+# Análisis léxico, sintáctico y semántico, para despúes
+# ejecutar los cuádruplos generados
+
 
 #Lexer rules
 
@@ -72,7 +76,7 @@ tokens = (
     'RANDOM',
     'CIRCULO',
 
-    #Regex
+    # Tokens con expresiones regulares
     'ID',
     'CTEI',
     'CTEF',
@@ -97,7 +101,8 @@ t_DIVISION = r'/'
 t_MAYOR = r'>'
 t_MENOR = r'<'
 
-
+# Funciones para las expresiones regulares
+# Con más de un caracter
 def t_PROGRAMA(t):
     r'programa'
     return t
@@ -281,6 +286,7 @@ def t_CTESTRING(t):
     return t
 
 #Caracteres ignorados
+# Espacio y tabulador
 t_ignore = ' \t'
 
 def t_ENDLINE(t):
@@ -295,19 +301,26 @@ def t_error(t):
 #Build lexer
 import ply.lex as lex
 lexer = lex.lex()
+
+# Importa las clases necesarias para el compilador
 from register import Register
 from quadruple_register import QuadrupleRegister
 from op_codes import OpCodes
 from virtual_machine import VirtualMachine
 
-#Parsing rules
+# Inicializacion de clases
 register = Register()
 quadruple_reg = QuadrupleRegister(lexer)
 register.set_address_handler(quadruple_reg.address_handler)
 main_goto_quadruple = 0
 
+
+#Parsing rules
+
+# Sintaxis principal de todo el programa
 def p_programa(p):
     'programa : dec_programa progvar main_goto progfunc main_block'
+
     print("/////////////Programa terminado con exito///////////////")
     print(" ######### Register table  ###########")
     register.print_table()
@@ -319,13 +332,18 @@ def p_programa(p):
     quadruple_reg.print_name_quadruples()
     print('########## Quadruple dirs ###########')
     quadruple_reg.print_quadruples()
+
     print('##')
     register.function_list[0]['size_dict'] = register.address_handler.return_global_size_dict()
+    # Cosigue el directorio de funciones de la clase register
     dir_funciones = register.function_list
+    # Guarda la tabla de constantes que se tenía en el quadruple_register
     constant_table = quadruple_reg.constant_list
+    # Guarda la lista de cuádruplos generados
     quadruple_list = quadruple_reg.quadruple_list
+
     quadruples = []
-    #get only dirs in quadruples
+    # Guarda sólo las direcciones en los cuádruplos
     for quadruple in quadruple_list:
         oper = quadruple['operator']
         op_1 = quadruple['operand_1']
@@ -342,65 +360,82 @@ def p_programa(p):
             res = res['address']
         quadruple_new = dict(operator=oper, operand_1=op_1, operand_2=op_2, result=res)
         quadruples.append(quadruple_new)
+    # Genera un último cuádruplo al final que termina el programa
     quadruple_new = dict(operator="end", operand_1=None, operand_2=None, result=None)
     quadruples.append(quadruple_new)
+    # Inicializa la máquina virtual y ejecuta los cuádruplos
     vm = VirtualMachine(quadruples, constant_table, dir_funciones)
     vm.execute_code()
     pass
 
+# El ID del programa se guarda en el registro de directorio de procedimientos
 def p_dec_programa(p):
     'dec_programa : PROGRAMA ID ENDLINE'
     #register.create(p[2])
     register.create("main")
     pass
 
+# Declaración de variables
 def p_progvar(p):
     '''progvar : var ENDLINE progvar
             | vacio'''
     pass
 
+# Se crea el primer cuadruplo, que tendrá un goto
+# a la primera instrucción del main
 def p_main_goto(p):
     'main_goto :'
     register.main_goto_quadruple = quadruple_reg.get_next_quadruple()
     quadruple_reg.generate(OpCodes.GOTO, None, None, None)
 
+# Declaración de funciones
 def p_progfunc(p):
     '''progfunc : function progfunc
             | vacio'''
     pass
 
+# Declaración de variables completas
 def p_var(p):
     'var : VAR ID DOSPUNTOS type add_var arrdec varasign'
     # print("Nueva variable-- ID: " + p[2] + "   Tipo: " + p[6] + "   Valor: " + str(p[4]))
     pass
 
+# Checa si la variable es un arreglo
 def p_arrdec(p):
     '''arrdec : arrdec1
             | vacio'''
 
+# Declaración del arreglo con su dimensión correspondiente
+# Se crean en el registro las direcciones del arreglo
 def p_arrdec1(p):
     'arrdec1 : CORCHETEI CTEI CORCHETED'
     variable = p[-1]
     register.create_array(variable['name'], p[2])
     register.address_handler.increment_address_count(variable['type'], register.current_scope, p[2])
 
+# Agrega la variable encontrada con su id y tipo
 def p_add_var(p):
     'add_var :'
     p[0] = register.add_variable(p[-3], p[-1])
     pass
 
+# Agrega la variable al stack de operandos
 def p_push_var(p):
     'push_var :'
     variable = register.get_variable(p[-5])
     quadruple_reg.push_operand(variable)
     pass
 
+# Asigna un valor a una variable que se esta declarando
+# Checa si los tipos de variable coinciden
 def p_varasign(p):
     '''varasign : push_var ASIGNACION push_operator ssexp
             | vacio'''
     quadruple_reg.assignment_check()
     pass
 
+# Tipos de variable
+# Entero, flotante, string o bool
 def p_type(p):
     '''type : INT
             | FLOAT
@@ -409,23 +444,29 @@ def p_type(p):
     p[0] = p[1]
     pass
 
+# Bloque principal del programa
 def p_main_block(p):
     'main_block : fill_goto block1'
     pass
-
+# Llena el primer cuádruplo del main con el número de cuádruplo de la
+# primera instruccion del main
 def p_fill_goto(p):
     'fill_goto :'
     quadruple_reg.fill_quadruple(register.main_goto_quadruple, quadruple_reg.get_next_quadruple())
 
+# Endline opcional
 def p_optional_endline(p):
     '''optional_endline : ENDLINE
                         | vacio'''
 
+# Ciclo de estatutos que pueden estar en un bloque
 def p_block1(p):
     '''block1 : statute block1
             | vacio'''
     pass
 
+# Los estatutos que pueden estar en un bloque son asignación, declaración de variables,
+# Condicionales, ciclos while o repeat, llamadas a funciónes y primitivas del lenguaje
 def p_statute(p):
     '''statute : assignment ENDLINE
             | var ENDLINE
@@ -436,38 +477,49 @@ def p_statute(p):
             | primitive_func ENDLINE'''
     pass
 
+# Llamada a una funcion completa, con id y numero de parámetros
+# Se limpian las variables que se van a usar y se genera un
+# cuádruplo de retorno
 def p_function(p):
     'function : FUNC ID dec_func PARENTESISI dec_varloc params PARENTESISD function_type func_block'
-    # register.function_list[register.current_scope]['size_dict'] = register.address_handler.return_size_dict()
     register.clear_variables()
     quadruple_reg.generate_return_action()
     pass
 
+# Se agrega la funcion con su id al registro de funciones
 def p_dec_func(p):
     'dec_func :'
     register.add_function(p[-1])
     pass
 
+# Checa si existen parámetros
 def p_params(p):
     '''params : params1
             | vacio'''
     pass
 
+# Checa el id y el tipo de parámetro que se está dando
 def p_params1(p):
     'params1 : ID DOSPUNTOS type add_param params2'
     pass
 
+# Agrega el parámetro para checar si coincide con su declaración
+# y para darle el valor al parámetro
 def p_add_param(p):
     'add_param : '
     register.add_param_counter += 1
     register.add_function_param(p[-3], p[-1])
     pass
 
+# Checa si hay más parámetros
 def p_params2(p):
     '''params2 : COMA params1
             | vacio'''
     pass
 
+#Checa el tipo de funcion y se agrega al registro de funiones su tipo,
+# Si no se declara algun tipo, se guarda como void
+# Se guarda el cuádruplo siguiente en el el registro de función
 def p_function_type(p):
     '''function_type : DOSPUNTOS type
             | vacio'''
@@ -479,15 +531,19 @@ def p_function_type(p):
     register.set_starting_quadruple(quadruple)
     pass
 
+# Bloque de una funcion
 def p_func_block(p):
     'func_block : LLAVEI optional_endline func_block1 LLAVED optional_endline'
     pass
 
+#  Loop de estatutos de una funcion
 def p_func_block1(p):
     '''func_block1 : func_statements func_block1
             | vacio'''
     pass
 
+# Los estatutos para una funcion son asignacion, declaración de variables,
+# Condicionales, ciclos while y repeat, llamadas a funcion y primitivas
 def p_func_statements(p):
     '''func_statements : assignment ENDLINE
             | var ENDLINE
@@ -499,6 +555,8 @@ def p_func_statements(p):
             | return ENDLINE'''
     pass
 
+# Return de una función. Se guarda la función actual para guardarla en
+# el scope global
 def p_return(p):
     'return : REGRESA ssexp'
     function = register.function_list[register.current_scope]
@@ -511,6 +569,7 @@ def p_dec_varloc(p):
     # print("Se crea la tabla de variables local. Tabla actual: " + p[-3])
     pass
 
+# Asignación de un valor a una variable previamente declarada
 def p_assignment(p):
     'assignment : ID push_id arrsino ASIGNACION push_operator ssexp'
     quadruple_reg.assignment_check()
@@ -526,6 +585,7 @@ def p_ssexp_check(p):
     quadruple_reg.ssexp_check()
     pass
 
+# La super super expresion contiene and y or, se agregan a la pila de operadores
 def p_ssexp2(p):
     '''ssexp2 : AND push_operator sexp ssexp_check
             | OR push_operator sexp ssexp_check
@@ -537,6 +597,7 @@ def p_sexp(p):
     'sexp : exp sexp2'
     pass
 
+# La super expresion contine los operadores binarios >, <, !=, ==, >= y <=
 def p_sexp2(p):
     '''sexp2 : MAYOR push_operator exp sexp_check
             | MENOR push_operator exp sexp_check
@@ -562,6 +623,7 @@ def p_exp_check(p):
     quadruple_reg.exp_check()
     pass
 
+# La expresion tiene los operadores de + y -
 def p_exp1(p):
     '''exp1 : SUMA push_operator exp
             | RESTA push_operator exp
@@ -578,17 +640,21 @@ def p_term_check(p):
     quadruple_reg.term_check()
     pass
 
+# Los terminos tienen los operadores de * y /
 def p_term1(p):
     '''term1 : MULTIP push_operator term
             | DIVISION push_operator term
             | vacio'''
     pass
 
+# Se agrega el operador a la pila de operadores
 def p_push_operator(p):
     'push_operator :'
     quadruple_reg.push_operator(p[-1])
 
 # Factor
+# Un factor puede ser una expresión entre paréntesis, una constante,
+# una variable, una llamada a función o una primitiva
 def p_factor(p):
     '''factor : PARENTESISI push_fake_bottom ssexp PARENTESISD pop_fake_bottom
             | varconst
@@ -597,6 +663,7 @@ def p_factor(p):
             | primitive_func ENDLINE'''
     pass
 
+# Agrega el retorno de la función, con su tipo y valor a la pila de retornos de función
 def p_push_function_return(p):
     'push_function_return :'
     function = p[-1]
@@ -604,88 +671,107 @@ def p_push_function_return(p):
     function_variable = register.get_variable(function['name'])
     quadruple_reg.push_function_return(return_type, function_variable)
 
+# Se agrega el ID de la variable a la pila de operandos
 def p_push_id(p):
     'push_id :'
     variable = register.get_variable(p[-1])
     quadruple_reg.push_operand(variable)
     pass
 
+# Agrega un fondo falso a la pila de operadores para poder manejar paréntesis y llaves
 def p_push_fake_bottom(p):
     'push_fake_bottom :'
     quadruple_reg.push_fake_bottom()
     pass
 
+# Quita un fondo falso de la pila de operadores
 def p_pop_fake_bottom(p):
     'pop_fake_bottom :'
     quadruple_reg.pop_fake_bottom()
     pass
 
+# Estatuto de condicion si, sino
 def p_condition(p):
     'condition : SI PARENTESISI ssexp PARENTESISD start_if_check control_block cond1 end_if_check'
     pass
 
+# Checa el inicio del if en el quadruple register
 def p_start_if_check(p):
     'start_if_check :'
     quadruple_reg.begin_if_check()
     pass
 
+# Termina el if después todo el bloque
 def p_end_if_check(p):
     'end_if_check :'
     quadruple_reg.end_if_check()
     pass
 
+# Checa si hay un sino
 def p_cond1(p):
     '''cond1 : SINO else_check control_block
             | vacio'''
     pass
 
+# Checa el sino en el quadruple register
 def p_else_check(p):
     'else_check :'
     quadruple_reg.else_check()
     pass
 
+# Declaración de un ciclo while
 def p_while(p):
     'while : MIENTRAS start_while_check PARENTESISI ssexp PARENTESISD mid_while_check control_block end_while_check'
     pass
 
+# Inicia el ciclo while en el quadruple register
 def p_start_while_check(p):
     'start_while_check :'
     quadruple_reg.begin_while_check()
     pass
 
+# Checa el ciclo while después de la expresión y paréntesis
 def p_mid_while_check(p):
     'mid_while_check :'
     quadruple_reg.middle_while_check()
     pass
 
+# Checa el final del while en el quadruple register
 def p_end_while_check(p):
     'end_while_check :'
     quadruple_reg.end_while_check()
     pass
 
+# Declaración del ciclo repetir
 def p_loop(p):
     'loop : REPETIR PARENTESISI ssexp PARENTESISD start_repeat_check control_block end_repeat_check'
     pass
 
+# Inicia el ciclo repetir después de la expresión y paréntesis
 def p_start_repeat_check(p):
     'start_repeat_check :'
     quadruple_reg.begin_repeat_check()
     pass
 
+# Termina el ciclo repetir al final del bloque
 def p_end_repeat_check(p):
     'end_repeat_check :'
     quadruple_reg.end_repeat_check()
     pass
 
+# El bloque de control de estatutos y condicionales
 def p_control_block(p):
     'control_block : LLAVEI optional_endline control_block1 LLAVED'
     pass
 
+# Loop de los estatutos de control
 def p_control_block1(p):
     '''control_block1 : control_statements control_block1
             | vacio'''
     pass
 
+# Los estatutos de control pueden ser asignaciones, condicionales, cilos while y repetir,
+# Llamada a función o primitivas
 def p_control_statements(p):
     '''control_statements : assignment ENDLINE
             | condition ENDLINE
@@ -696,6 +782,8 @@ def p_control_statements(p):
             | return ENDLINE'''
     pass
 
+# Llamada a una función, Al final de la llamada genera un cuádruplo de gosub con la
+# direccion de inicio de la función obtenida por el register.
 def p_function_call(p):
     '''function_call : ID function_check PARENTESISI generate_era args PARENTESISD'''
     start_dir = register.get_function_starting_quadruple()
@@ -709,6 +797,7 @@ def p_function_call(p):
     quadruple_reg.print_name_quadruples()
     pass
 
+# Se checa que la función exista con el ID dado
 def p_function_check(p):
     'function_check :'
     function_name = p[-1]
@@ -716,6 +805,8 @@ def p_function_check(p):
     quadruple_reg.push_fake_bottom()
     pass
 
+# Genera un cuádruplo era con el nombre de la función y se inicializa el
+# contador de parámetros en 0
 def p_generate_era(p):
     'generate_era :'
     function_name = register.get_current_function()['name']
@@ -724,15 +815,20 @@ def p_generate_era(p):
     print(register.get_current_function())
     pass
 
+# Checa si hay argumentos en la llamada a función
 def p_args(p):
     '''args : args1
             | vacio'''
     pass
 
+
 def p_args1(p):
     'args1 : ssexp increment_counter init_argument args2'
     pass
 
+# Se guarda el argumento,
+# Checa si el tipo de argumento que se esta dando es el esperado
+# y verifica que el numero de argumentos sea correcto
 def p_init_argument(p):
     'init_argument :'
     arg_type = register.get_expected_arg_type()
@@ -741,16 +837,19 @@ def p_init_argument(p):
     quadruple_reg.verify_and_generate_argument(arg_type, params_counter, param_max)
     pass
 
+# Checa si hay mas argumentos
 def p_args2(p):
     '''args2 : COMA args1
             | vacio'''
     pass
 
+# Incrementa el contador de parametros
 def p_increment_counter(p):
     'increment_counter : '
     register.params_counter += 1
     pass
 
+# las constantes pueden ser string, flotante, enteras o booleanas
 def p_varconst(p):
     '''varconst : CTESTRING push_string_literal
             | CTEF push_float_literal
@@ -758,46 +857,54 @@ def p_varconst(p):
             | boolvalue push_bool_literal'''
     pass
 
+# Se agrega el registro de la constante string
 def p_push_string_literal(p):
     'push_string_literal :'
     quadruple_reg.push_string_literal(str(p[-1]))
     pass
 
+# Se agrega el registro de la constante etera
 def p_push_int_literal(p):
     'push_int_literal :'
     quadruple_reg.push_int_literal(int(p[-1]))
     pass
 
+# Se agrega el registro de la constante flotante
 def p_push_float_literal(p):
     'push_float_literal :'
     quadruple_reg.push_float_literal(float(p[-1]))
     pass
 
+# Se agrega el registro de la constante booleana
 def p_push_bool_literal(p):
     'push_bool_literal :'
     quadruple_reg.push_bool_literal(p[-1])
     pass
 
+# Checa si es arreglo la variable
 def p_arrsino(p):
     '''arrsino : arraccess
             | vacio'''
     pass
 
+# Accesa a la direccion del arreglo dado
 def p_arraccess(p):
     'arraccess : CORCHETEI array_check ssexp CORCHETED'
     quadruple_reg.generate_array_access(p[2]['address'], p[2]['upper_limit'])
 
+# Verifica el arreglo
 def p_array_check(p):
     'array_check :'
     p[0] = quadruple_reg.array_check()
 
-
+#Posibles valores del bool (verdadero y falso)
 def p_boolvalue(p):
     '''boolvalue : VERDADERO
             | FALSO'''
     p[0] = p[1]
     pass
 
+#Lista de funciones primitivas con sus parámetros
 def p_primitive_func(p):
     '''primitive_func : ESCRIBE PARENTESISI ssexp PARENTESISD generate_write
             | LEE PARENTESISI ssexp PARENTESISD generate_read
@@ -821,6 +928,8 @@ def p_primitive_func(p):
             | RANDOM PARENTESISI ssexp PARENTESISD generate_random
             | CIRCULO PARENTESISI ssexp COMA ssexp PARENTESISD generate_circle '''
     pass
+
+# Genera los cuádruplos de las siguientes primitivas
 
 def p_generate_read(p):
     'generate_read :'
@@ -906,11 +1015,13 @@ def p_generate_circle(p):
     'generate_circle :'
     quadruple_reg.generate_circle()
 
+
+# Expresion para un vacio
 def p_vacio(p):
     'vacio :'
     pass
 
-#Manejo de errores
+#Manejo de errores de sintaxis
 def p_error(p):
     print("Syntax error at line " + str(p.lexer.lineno) + " : Unexpected token  " + str(p.value) )
     sys.exit
@@ -919,6 +1030,7 @@ def p_error(p):
 import ply.yacc as yacc
 parser = yacc.yacc()
 
+# Abre el archivo del input para leerlo y analizarlo
 def main(argv):
     filename = sys.argv[1]
     file = open(filename)
